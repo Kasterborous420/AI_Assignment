3d5947fd23affd7c6f7391f6210c9c3e966113d8
unknown <140189H@M615G203.sidm.student.local>
2017-01-09 17:31:51 +0800

    Alex is a stupid nigger

---
+++ Patrol_Solution/Waiter.cpp
@@ -0,0 +1,11 @@
+#include "Waiter.h"
+
+
+CWaiter::CWaiter()
+{
+}
+
+
+CWaiter::~CWaiter()
+{
+}
---
+++ Patrol_Solution/Waiter.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "MyVector.h"
+#include <vector>
+using std::vector;
+class CWaiter
+{
+	// Waiter related
+	enum WAITER_STATE
+	{
+		E_WAITER_SERVE,
+		E_WAITER_IDLE,
+		E_WAITER_PICKUP,
+		E_WAITER_PICKUPCUSTOMER,
+		E_WAITER_MOVE,
+		E_WAITER_MAX
+	};
+private:
+	WAITER_STATE waiterState;
+	MyVector waiterPos;
+	MyVector waiterSpawn;
+	bool foodReady;
+	bool availableCustomers;
+	bool customerPickup;
+	bool isBusy;
+	const float waiterSpeed = 0.02f;
+
+
+public:
+	CWaiter();
+	~CWaiter();
+
+	vector <MyVector> waiterWayPoints;
+
+	WAITER_STATE GetState();
+	void SetState(WAITER_STATE newState);
+
+	MyVector GetPos();
+	void SetPos(MyVector newPos);
+
+	MyVector GetSpawn();
+	void SetSpawn(MyVector newSpawn);
+
+	bool GetFoodReady();
+	void SetFoodReady(bool newFoodReady);
+
+	bool GetAvailableCustomers();
+	void SetAvailableCustomers(bool newAvailableCustomers);
+
+	bool GetCustomerPickup();
+	void SetCustomerPickup(bool newCustomerPickup);
+	
+	bool GetBusy();
+	void SetBusy(bool busyornot);
+
+	const float GetSpeed();
+
+
+
+};
+
